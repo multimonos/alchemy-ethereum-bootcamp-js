dd034a2737f12f0a443b19b03d603a2c23d1fcbe
Craig Hopgood <searaig@gmail.com>
2024-07-25 22:11:51 -0400

    build rainbow table once

--- src/brute-force-hashing.js
+++ src/brute-force-hashing.js
@@ -4,19 +4,21 @@ import { sha256 } from "ethereum-cryptography/sha256";
 
 export const COLORS = [ 'red', 'green', 'blue', 'yellow', 'pink', 'orange' ];
 
-export function findColor( hash ) {
+const rainbowTable = COLORS
+    .map( utf8ToBytes )
+    .map( tap( "utf8tobytes" ) )
+    .map( sha256 )
+    .map( tap( "sha256" ) )
+    .map( toHex )
+    .map( tap( "toHex" ) )
 
-    const rainbow = COLORS
-        .map( utf8ToBytes )
-        .map( tap( "utf8tobytes" ) )
-        .map( sha256 )
-        .map( tap( "sha256" ) )
-        .map( toHex )
-        .map( tap( "toHex" ) )
+
+export function findColor( hash ) {
 
     const needle = toHex( hash )
 
-    const index = rainbow.findIndex( x => needle === x )
+    const index = rainbowTable
+        .findIndex( x => needle === x )
 
     return COLORS[index]
 }
